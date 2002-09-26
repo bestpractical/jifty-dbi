320493ee1dee33fe6ef089814ff22aef415698de
Jesse Vincent <jesse@bestpractical.com>
2002-09-26 03:22:30 -0400

    Initial port to DBD::SQLite

---
+++ SearchBuilder/Handle/SQLite.pm
@@ -0,0 +1,73 @@
+
+package DBIx::SearchBuilder::Handle::SQLite;
+use DBIx::SearchBuilder::Handle;
+@ISA = qw(DBIx::SearchBuilder::Handle);
+
+use vars qw($VERSION @ISA $DBIHandle $DEBUG);
+use strict;
+
+=head1 NAME
+
+  DBIx::SearchBuilder::Handle::SQLite -- a mysql specific Handle object
+
+=head1 SYNOPSIS
+
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+Jesse Vincent, jesse@fsck.com
+
+=head1 SEE ALSO
+
+perl(1), DBIx::SearchBuilder
+
+=cut
+
+# {{{ sub Insert
+
+=head2 Insert
+
+Takes a table name as the first argument and assumes that the rest of the arguments
+are an array of key-value pairs to be inserted.
+
+
+If the insert succeeds, returns the id of the insert, otherwise, returns
+a Class::ReturnValue object with the error reploaded.
+
+=cut
+
+sub Insert  {
+    my $self = shift;
+    my %args = (id => undef,
+                @_);
+    my $sth = $self->SUPER::Insert(@_);
+    if (!$sth) {
+	    return ($sth);
+     }
+
+    $self->{'id'}=$self->dbh->func('last_insert_rowid');
+ 
+    warn "$self no row id returned on row creation" unless ($self->{'id'});
+    return( $self->{'id'}); #Add Succeded. return the id
+  }
+
+# }}}
+
+
+=head2 CaseSensitive 
+
+Returns undef, since mysql's searches are not case sensitive by default 
+
+=cut
+
+sub CaseSensitive {
+    my $self = shift;
+    return(undef);
+}
+
+
+# }}}
+
+
