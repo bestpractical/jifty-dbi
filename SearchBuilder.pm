b7706978a96ef8321a956e1cd2d6bfd67ffb7550
David Glasser <glasser@bestpractical.com>
2005-06-01 22:21:47 +0000

    (empty commit message)

--- SearchBuilder.pm
+++ SearchBuilder.pm
@@ -196,15 +196,15 @@ sub _DoSearch {
     # If we're about to redo the search, we need an empty set of items
     delete $self->{'items'};
 
-    eval {
-        # TODO: finer-grained eval and checking.
-        my $records = $self->_Handle->SimpleQuery($QueryString);
+    my $records = $self->_Handle->SimpleQuery($QueryString);
+    return 0 unless $records;
 
-        while ( my $row = $records->fetchrow_hashref() ) {
-            my $item = $self->NewItem();
-            $item->LoadFromHash($row);
-            $self->AddRecord($item);
-        }
+    while ( my $row = $records->fetchrow_hashref() ) {
+	my $item = $self->NewItem();
+	$item->LoadFromHash($row);
+	$self->AddRecord($item);
+    }
+    return $self->_RecordCount if $records->err;
 
     $self->{'must_redo_search'} = 0;
 
