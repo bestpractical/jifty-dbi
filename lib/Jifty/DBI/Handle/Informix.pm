4926ce3165fc0e6c13995e66de5097f3f202f03b
Jesse Vincent <jesse@bestpractical.com>
2005-12-09 01:06:39 -0500

    * Ported fixes from SB to Informix handle git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/Jifty-DBI/trunk@352 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/DBI/Handle/Informix.pm
+++ lib/Jifty/DBI/Handle/Informix.pm
@@ -121,17 +121,26 @@ takes an incomplete SQL SELECT statement and massages it to return a DISTINCT re
 
 =cut
 
-sub distinct_query {
-    my $self         = shift;
-    my $statementref = shift;
-    my $table        = shift;
-
-    # Wrapper select query in a subselect as Informix doesn't allow
-    # DISTINCT against CLOB/BLOB column types.
-    $$statementref
-        = "SELECT * FROM $table main WHERE id IN ( SELECT DISTINCT main.id FROM $$statementref )";
-
-}
+ sub distinct_query {
+     my $self = shift;
+     my $statementref = shift;
+    my $sb = shift;
+    my $table = $sb->table;
+ 
+    if ($sb->_order_clause =~ /(?<!main)\./) {
+        # Don't know how to do ORDER BY when the DISTINCT is in a subquery
+        warn "Query will contain duplicate rows; don't how how to ORDER BY across DISTINCT";
+       $$statementref = "SELECT main.* FROM $$statementref";
+    } else {
+        # Wrapper select query in a subselect as Informix doesn't allow
+        # DISTINCT against CLOB/BLOB column types.
+        $$statementref = "SELECT * FROM $table main WHERE id IN ( SELECT DISTINCT main.id FROM $$statementref )";
+    }
+    $$statementref .= $sb->_group_clause;
+    $$statementref .= $sb->_order_clause;
+ }
+ 
+ 
 
 1;
 
