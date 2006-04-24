ae455bc480d54991a285018fbbacaa06d7f37694
Eric Wilhelm <ewilhelm@cpan.org>
2006-04-24 08:33:45 -0700

    lib/Jifty/DBI/Record.pm - added Class::ReturnValue's to disallowed accessors git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/Jifty-DBI/trunk@1006 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/DBI/Record.pm
+++ lib/Jifty/DBI/Record.pm
@@ -305,9 +305,15 @@ sub _init_methods_for_column {
     elsif (
         UNIVERSAL::isa( $column->refers_to, "Jifty::DBI::Collection" ) )
     { # XXX elw: collections land here, now what?
-      $subref = sub {
-          return (0, "Collection column '$column_name' not writable")
-      };
+      my $ret = Class::ReturnValue->new();
+      my $message = "Collection column '$column_name' not writable";
+      $ret->as_array( 0, $message );
+      $ret->as_error(
+          errno        => 3,
+          do_backtrace => 0,
+          message      => $message
+      );
+      $subref = sub { return ( $ret->return_value ); };
     }
     else {
       $subref = sub {
