a2bc1638035dd313d219f57dddfd76a3dce04597
Audrey Tang <audreyt@audreyt.org>
2007-01-27 04:41:14 +0000

    * Fix "refers_to" bug when cloning virtual columns with column name matching /_id$/. git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/Jifty-DBI/branches/od@2631 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/DBI/Schema.pm
+++ lib/Jifty/DBI/Schema.pm
@@ -310,15 +310,14 @@ sub _init_column {
                 my $aliased_as = $1;
                 my $virtual_column = $from->add_column($aliased_as);
 
-                # XXX FIXME I think the next line is wrong, but things
-                # explode without it -- mostly because we unique-key
-                # on name instead of some conbination of name and
-                # alias_for_column in a couple places
-                $virtual_column->name( $name );
-                $virtual_column->aliased_as($aliased_as);
-#                $_->apply($virtual_column) for @args;
+                # Clone ourselves into the virtual column
+                %$virtual_column = %$column;
+
                 $column->refers_to(undef);
+
+                $virtual_column->aliased_as($aliased_as);
                 $virtual_column->alias_for_column($name);
+
                 $from->_init_methods_for_column($virtual_column);
             }
             $column->by('id') unless $column->by;
