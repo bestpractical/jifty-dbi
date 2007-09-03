707706e996b34d29b1413adb3f89889a204c7eb9
Andrew Sterling Hanenkamp <sterling@hanenkamp.com>
2007-09-02 21:32:04 -0500

    Added documentation for using @EXPORT with mixins. git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/Jifty-DBI/trunk@4034 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/DBI/Record/Plugin.pm
+++ lib/Jifty/DBI/Record/Plugin.pm
@@ -15,6 +15,11 @@ Jifty::DBI::Record::Plugin - Record model mixins for Jifty::DBI
   package MyApp::FavoriteColor;
   use base qw/ Jifty::DBI::Record::Plugin /;
 
+  # Define which methods you want to put in the host model
+  our @EXPORT = qw(
+      favorite_complementary_color
+  );
+
   use Jifty::DBI::Schema;
   use Jifty::DBI::Record schema {
       column favorite_color =>
@@ -22,6 +27,16 @@ Jifty::DBI::Record::Plugin - Record model mixins for Jifty::DBI
           label is 'Favorite Color',
           valid_values are qw/ red green blue yellow /;
   };
+
+  sub favorite_complementary_color {
+      my $self = shift; # whatever host object thing we've mixed with
+      my $color = $self->favorite_color;
+      return $color eq 'red'    ? 'green'
+           : $color eq 'green'  ? 'red'
+           : $color eq 'blue'   ? 'orange'
+           : $color eq 'yellow' ? 'purple'
+           :                      undef;
+  }
 
   # Use the mixin
   package MyApp::Model::User;
@@ -44,6 +59,14 @@ Jifty::DBI::Record::Plugin - Record model mixins for Jifty::DBI
       return "The favorite color of $name is $color.";
   }
 
+  sub name_and_complementary_color {
+      my $self  = shift;
+      my $name  = $self->name;
+      my $color = $self->favorite_complementary_color;
+
+      return "The complement of $name's favorite color is $color.";
+  }
+
 =head1 DESCRIPTION
 
 By using this package you may provide models that are built from one or more mixins. In fact, your whole table could be defined in the mixins without a single column declared within the model class itself.
@@ -62,6 +85,27 @@ To build a mixin, just create a model that inherits from this package, C<Jifty::
           valid are qw/ african european /,
           default is 'african';
   };
+
+=head3 @EXPORT
+
+A mixin may define an C<@EXPORT> variable, which works exactly as advertised in L<Exporter>. That is, given the name of any methods or variable names in the mixin, the host model will gain those methods. 
+
+  our @EXPORT = qw( autocomplete_swallow_type );
+
+  sub autocomplete_swallow_type {
+      my $self  = shift;
+      my $value = quotemeta(shift);
+
+      # You should probably find a better way than actually doing this...
+
+      my @values;
+      push @values, 'african'  if 'african'  =~ /$value/;
+      push @values, 'european' if 'european' =~ /$value/;
+
+      return @values;
+  }
+
+That way if you have any custom methods you want to throw into the host model, just define them in the mixin and add them to the C<@EXPORT> variable.
 
 =head3 register_triggers
 
