15a1311369ac51b41e4879c169d529f3b5f01b25
Andrew Sterling Hanenkamp <sterling@hanenkamp.com>
2007-11-29 14:40:41 -0600

    Setting a default for RECORD_MIXINS and altering the mk_classdata() decls to avoid any confusion with the mk_accessors() interface. git-svn-id: svn+ssh://svn.bestpractical.com/svn/jifty.org/Jifty-DBI/trunk@4572 e84bef0a-9b06-0410-84ba-c4c9edb13aeb

--- lib/Jifty/DBI/Record.pm
+++ lib/Jifty/DBI/Record.pm
@@ -17,12 +17,12 @@ use base qw/
 
 our $VERSION = '0.01';
 
-Jifty::DBI::Record->mk_classdata(qw/COLUMNS/);
-Jifty::DBI::Record->mk_classdata(qw/TABLE_NAME/);
-Jifty::DBI::Record->mk_classdata(qw/_READABLE_COLS_CACHE/);
-Jifty::DBI::Record->mk_classdata(qw/_WRITABLE_COLS_CACHE/);
-Jifty::DBI::Record->mk_classdata(qw/_COLUMNS_CACHE/);
-Jifty::DBI::Record->mk_classdata(qw/RECORD_MIXINS/);
+Jifty::DBI::Record->mk_classdata('COLUMNS');
+Jifty::DBI::Record->mk_classdata('TABLE_NAME');
+Jifty::DBI::Record->mk_classdata('_READABLE_COLS_CACHE');
+Jifty::DBI::Record->mk_classdata('_WRITABLE_COLS_CACHE');
+Jifty::DBI::Record->mk_classdata('_COLUMNS_CACHE');
+Jifty::DBI::Record->mk_classdata('RECORD_MIXINS' => []);
 
 =head1 NAME
 
